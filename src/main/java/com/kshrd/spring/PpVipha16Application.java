d8644ec0a9c68066bfb4464ac974a34eef2365b3
√iphÅ <faviphaseng@gmail.com>
2017-06-22 15:28:43 +0700

    first commit

---
+++ src/main/java/com/kshrd/spring/PpVipha16Application.java
@@ -0,0 +1,12 @@
+package com.kshrd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpVipha16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpVipha16Application.class, args);
+	}
+}
